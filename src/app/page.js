61d9d3ca46f0ac685444c790aefaf70314ab609c
Pedro Henrique <pedrohenriquefesi@gmail.com>
2024-08-19 22:17:11 -0300

    mudanças nos textos e botões das etapas adicionados e posicionados

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import IconArrowLeftShort from "./icons/IconArrowLeftShort";
 import IconArrowRightShort from "./icons/IconArrowRightShort";
 
 export default function Home() {
-  const stepNames = ['Componentes do circuito', 'Etapa de busca', 'Etapa de decodificação', 'Etapa de execução', 'Etapa de acesso à memória', 'Etapa de escrita'];
+  const stepNames = ['Componentes do circuito', 'Etapa de busca', 'Etapa de decodificação', 'Etapa de execução', 'Etapa de acesso à memória', 'Etapa de escrita no banco de registradores'];
 
   const [buttonSelected, setButtonSelected] = useState(0);
 
@@ -36,9 +36,12 @@ export default function Home() {
               fill
               objectFit="contain"
             />
-            <button onClick={() => setButtonSelected(1)} className={`bg-blue-600/15 absolute w-[30%] h-[20%] top-[10%] left-[25%] border-blue-600 ${buttonSelected == 1 && 'border-4'}`}></button>
-            <button onClick={() => setButtonSelected(2)} className={`bg-green-600/15 absolute w-[40%] h-[20%] top-[70%] left-[20%] border-green-600 ${buttonSelected == 2 && 'border-4'}`}></button>
-            <button onClick={() => setButtonSelected(3)} className={`bg-orange-600/15 absolute w-[30%] h-[30%] top-[15%] left-[60%] border-orange-600 ${buttonSelected == 3 && 'border-4'}`}></button>
+            <button onClick={() => setButtonSelected(1)} className={`bg-blue-600/15 absolute w-[34%] h-[80%] top-[10%] left-[2%] border-blue-600 ${buttonSelected == 1 && 'border-4'}`}></button>
+            <button onClick={() => setButtonSelected(2)} className={`bg-green-600/15 absolute w-[25%] h-[42%] top-[46%] left-[37%] border-green-600 ${buttonSelected == 2 && 'border-4'}`}></button>
+            <button onClick={() => setButtonSelected(3)} className={`bg-red-600/15 absolute w-[12%] h-[39%] top-[46%] left-[63%] border-red-600 ${buttonSelected == 3 && 'border-4'}`}></button>
+            <button onClick={() => setButtonSelected(3)} className={`bg-red-600/15 absolute w-[16%] h-[31%] top-[8%] left-[40%] border-red-600 ${buttonSelected == 3 && 'border-4'}`}></button>
+            <button onClick={() => setButtonSelected(4)} className={`bg-yellow-600/15 absolute w-[20%] h-[43%] top-[54%] left-[75.5%] border-yellow-600 ${buttonSelected == 4 && 'border-4'}`}></button>
+            <button onClick={() => setButtonSelected(5)} className={`bg-purple-600/15 absolute w-[60%] h-[5.5%] top-[40%] left-[37.5%] border-purple-600 ${buttonSelected == 5 && 'border-4'}`}></button>
           </div>
           <button onClick={() => setButtonSelected(0)} className="bg-[#3197C2BB] text-white text-lg py-2 px-4 rounded-full">Sobre os componentes</button>
         </div>
@@ -55,43 +58,59 @@ export default function Home() {
           <div className="w-full text-slate-800 flex flex-col gap-5 text-lg md:text-xl text-justify leading-7 md:leading-8">
             { buttonSelected == 0 && 
               <>
-                <p>
-                  PC: PC é uma sigla para Program Counter, ou Contador do Programa. Sua função é dizer em qual posição da memória está o dado que estamos buscando.
-                </p>
-                <p>
-                  MEMÓRIA: A memória guarda os dados que serão usados na execução de um programa. Uma memória tem várias posições, e em cada uma existe uma informação guardada. Podemos pensar nela como um armário com diversas gavetas. Em cada gaveta, há um dado guardado, e quando queremos um determinado dado, abrimos a gaveta onde ele está e o pegamos para usar. No circuito, podemos ver duas memórias, cada uma com um propósito: a MEMÓRIA DE INSTRUÇÕES, que, como seu nome indica, guarda as instruções que serão seguidas para a execução de uma determinada tarefa; e a MEMÓRIA DE DADOS, que guarda valores usados pelo programa.
-                </p>
-                <p>
-                  BANCO DE REGISTRADORES: o banco de registradores tem uma função parecida com a da memória: guardar dados usados pelo programa. Porém, é mais fácil acessá-lo que a memória, e todos os dados que serão usados devem primeiro ser trazidos da memória para o banco de registradores. Uma vez que isso é feitos, eles podem ser utilizados.
-                </p>
-                <p>
-                  ULA: ULA significa Unidade Lógica e Aritmética, e é ela quem realiza operações de adição, subtração, multiplicação e divisão, além de outras, como verificar se um número é maior que outro, se dois números são iguais, etc.
-                </p>
+                <div>
+                  <h3 className=" font-bold">PC (Program Counter - Contador do Programa):</h3>
+                  <p>
+                    O PC é como um marcador de páginas em um livro. Ele nos diz exatamente em qual página (ou posição da memória) estamos agora, e para onde devemos ir em seguida. Quando queremos ler uma nova instrução, é o PC que aponta para onde devemos ir na memória.
+                  </p>
+                </div>
+                <div>
+                  <h3 className=" font-bold">MEMÓRIA:</h3>
+                  <p>
+                    Imagine a memória como um grande armário cheio de gavetas. Cada gaveta guarda uma informação importante, seja uma instrução ou um dado. Quando o computador precisa de algo, ele "abre" a gaveta correta para pegar o que precisa.
+                  </p>
+                  <ul className="list-disc">
+                    <li className=" ml-10"><span className=" font-semibold">MEMÓRIA DE INSTRUÇÕES:</span> Esta parte do armário guarda o "manual de instruções" que o computador segue. Cada instrução diz ao computador o que fazer em seguida.</li>
+                    <li className=" ml-10"><span className=" font-semibold">MEMÓRIA DE DADOS:</span> Outra parte do armário, mas esta guarda os ingredientes (dados) que o computador usa para realizar suas tarefas. Quando o computador precisa de um valor específico, ele o busca aqui.</li>
+                  </ul>
+                </div>
+                <div>
+                  <h3 className=" font-bold">BANCO DE REGISTRADORES:</h3>
+                  <p>
+                    Pense no banco de registradores como uma prateleira pequena e rápida de acessar, onde o computador coloca as informações que ele vai usar imediatamente. Antes de trabalhar com qualquer dado, ele primeiro traz o dado da memória e o coloca nessa prateleira, para poder usá-lo rapidamente.
+                  </p>
+                </div>
+                <div>
+                  <h3 className=" font-bold">ULA (Unidade Lógica e Aritmética):</h3>
+                  <p>
+                    A ULA é a "calculadora" do computador. É ela que faz todas as contas, como somar ou subtrair, e também faz comparações, como verificar qual número é maior ou se dois números são iguais. Se o computador precisa fazer qualquer tipo de cálculo, ele pede ajuda à ULA.
+                  </p>
+                </div>
               </>
             }
             { buttonSelected == 1 && 
               <p>
-                Na etapa de busca da instrução, a memória é acessada na posição indicada pelo PC. O conteúdo armazenado nessa posição de memória é levado para o registrador de instruções (IR). O PC é, então, atualizado para a posição da próxima instrução, que depende do modo com que o computador foi planejado.
+                Nesta etapa, o computador busca a próxima instrução a ser executada. Para fazer isso, ele usa o PC (Contador do Programa) como um guia, que indica a posição exata na memória onde a instrução está armazenada. O conteúdo dessa posição é então carregado no registrador de instruções (IR). Uma vez que a instrução é carregada, o PC é atualizado para apontar para a próxima instrução, garantindo que o computador saiba para onde ir em seguida.
               </p>
             }
             { buttonSelected == 2 && 
               <p>
-                Na etapa de decodificação, o processador lê a instrução buscada na etapa anterior e determina o que será executado. É como se nessa etapa acontecesse o planejamento de como a tarefa será realizada. A instrução traz todas as informações necessárias para a execução, como: se há necessidade de acessar a memória, o banco de registradores, ou ambos; se a ULA será usada, e qual a operação aritmética a ser feita, dentre outras informações importantes. Com isso, a unidade de controle gera sinais que ativarão as partes dos circuitos que serão utilizadas nas próximas etapas.
+                Agora que o computador tem a instrução, ele precisa entender o que deve fazer. Nesta etapa, a instrução é "decodificada" – ou seja, o computador analisa e interpreta a instrução para determinar as ações necessárias. É como planejar as etapas de uma tarefa complexa. A decodificação revela se o computador precisa acessar a memória, o banco de registradores, ou utilizar a ULA (Unidade Lógica e Aritmética) para realizar operações como soma ou subtração. Com essa análise, a unidade de controle gera sinais que preparam os componentes do computador para a próxima fase.
               </p>
             }
             { buttonSelected == 3 && 
               <p>
-                Na etapa de execução, o que foi planejado na etapa anterior é, então, executado. Por exemplo, se a instrução indicava a necessidade de somar dois números que estavam armazenados em dois registradores, é neste momento que a soma será feita.
+                Esta é a fase em que o planejamento feito anteriormente é colocado em ação. Por exemplo, se a instrução decodificada indica que dois números devem ser somados, a ULA realizará essa operação agora. Tudo o que foi planejado na etapa de decodificação é executado nesta fase.
               </p>
             }
             { buttonSelected == 4 && 
               <p>
-                Na etapa de acesso à memória, acontece a leitura ou escrita de dados que estão guardados na memória. Se a instrução do momento diz para trazer um valor da memória para um dos registradores, ou escrever um valor que está em um registrador na memória, isso será feito agora.
+                Se a instrução exige que o computador leia ou escreva dados na memória, isso acontece nesta etapa. Por exemplo, se a instrução diz para buscar um valor armazenado na memória e carregá-lo em um registrador, ou para salvar um valor de um registrador na memória, é agora que o computador faz isso. Esta etapa garante que os dados necessários estejam disponíveis para o processamento.
               </p>
             }
             { buttonSelected == 5 && 
               <p>
-                Na etapa de escrita no banco de registradores, acontece o armazenamento do resultado de uma operação em um registrador. Depois de fazer uma multiplicação na ULA, por exemplo, o resultado deve ser guardado em algum lugar para poder ser utilizado posteriormente. Portanto, nesta etapa, este resultado é escrito em um registrador.
+                Depois que uma operação, como uma multiplicação, é realizada pela ULA, o resultado precisa ser guardado para ser usado mais tarde. Nesta etapa, o computador armazena o resultado em um registrador específico. Isso garante que o resultado esteja disponível para qualquer outra operação ou instrução que possa precisar dele no futuro.
               </p>
             }
           </div>
